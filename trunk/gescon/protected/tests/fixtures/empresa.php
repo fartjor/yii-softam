802a99d823169b8b46fb23f955daa33c6390a9c2
jairjr31 <jairjr31@54de4e50-f3c9-872d-4b73-0b5dc8d010b7>
2011-05-19 18:08:36 +0000

    Cadastro de Empresa finalizado

--- trunk/gescon/protected/tests/fixtures/empresa.php
+++ trunk/gescon/protected/tests/fixtures/empresa.php
@@ -3,24 +3,32 @@
 return array(
 	/*
 	'sample1'=>array(
-		'end_id' => '',
 		'emp_nome' => '',
 		'emp_cnpj' => '',
 		'emp_data_ingresso' => '',
 		'emp_site' => '',
 		'emp_email' => '',
 		'emp_cpf_socio_majoritario' => '',
-		'tel_id' => '',
+		'emp_fone1' => '',
+		'emp_fone2' => '',
+		'emp_uf' => '',
+		'emp_cidade' => '',
+		'emp_endereco' => '',
+		'emp_cep' => '',
 	),
 	'sample2'=>array(
-		'end_id' => '',
 		'emp_nome' => '',
 		'emp_cnpj' => '',
 		'emp_data_ingresso' => '',
 		'emp_site' => '',
 		'emp_email' => '',
 		'emp_cpf_socio_majoritario' => '',
-		'tel_id' => '',
+		'emp_fone1' => '',
+		'emp_fone2' => '',
+		'emp_uf' => '',
+		'emp_cidade' => '',
+		'emp_endereco' => '',
+		'emp_cep' => '',
 	),
 	*/
 );
